3e812cfeaa08d8536fb5bc9547389d2f4f3b1146
UtsavKc8084 <107475962+UtsavKc8084@users.noreply.github.com>
2022-06-15 03:17:15 +0545

    change

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+`use strict`;
 const form = document.querySelector(".contact-form");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   let name = document.querySelector("#name").value;
   let recEmail = document.querySelector("#recEmail").value;
-  let senEmail = document.querySelector("#sendEmail").value;
+  let senEmail = document.querySelector("#senEmail").value;
 
   let message = document.querySelector("#message").value;
   document.querySelector(".contact-form").reset();
@@ -11,9 +12,9 @@ form.addEventListener("submit", function (e) {
 });
 function sendEmail(name, recEmail, senEmail, message) {
   Email.send({
-    Host: "smtp.elasticemail.com",
+    Host: "smtp.gmail.com",
     Username: "vastu8084@gmail.com",
-    Password: "2E9B5C76171AEF3045E1381BF5E094BE9737",
+    Password: "1997@ugust",
     To: recEmail,
     From: senEmail,
     Subject: `Your Wish My Love`,
@@ -21,7 +22,7 @@ function sendEmail(name, recEmail, senEmail, message) {
                  EmailToLove:${recEmail}
                  EmailFromLove:${senEmail}
                  YourWish:${message}`,
-  });
+  }).then((message) => alert(message));
   // }).then((message) =>
   //   alert(
   //     `Happy Birthday Gomu 💘. I wish your every dream come true and alive in these year. Your all obastacle goes far away From your life. And I will be your smile of your face and love of your life. Every day is a new day but today is the specal day for us because it direct us towards our destiny in where we want to see eachother. I love 💘 you a lot gomu and i want to live my whole life with you so for the reason i will do works that make me your man and help us to move ahead togther with lots of love 💘. Happy Birthday Gomu 💘{Love Of My Life}`

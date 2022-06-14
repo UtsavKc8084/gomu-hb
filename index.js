423d9e22cf42cdf1a19646a6196ece2dfeaa3d7b
UtsavKc8084 <107475962+UtsavKc8084@users.noreply.github.com>
2022-06-15 04:54:30 +0545

    Update index.js

--- index.js
+++ index.js
@@ -19,10 +19,9 @@ function sendEmail(name, senEmail, message) {
     Body: `LoveName:${name}
                  EmailFromLove:${senEmail}
                  YourWish:${message}`,
-  }).then((message) => alert(message));
-  // }).then((message) =>
-  //   alert(
-  //     `Happy Birthday Gomu 💘. I wish your every dream come true and alive in these year. Your all obastacle goes far away From your life. And I will be your smile of your face and love of your life. Every day is a new day but today is the specal day for us because it direct us towards our destiny in where we want to see eachother. I love 💘 you a lot gomu and i want to live my whole life with you so for the reason i will do works that make me your man and help us to move ahead togther with lots of love 💘. Happy Birthday Gomu 💘{Love Of My Life}`
-  //   )
-  // );
+  }).then((message) =>
+    alert(
+      `Happy Birthday Gomu 💘. I wish your every dream come true and alive in these year. Your all obastacle goes far away From your life. And I will be your smile of your face and love of your life. Every day is a new day but today is the specal day for us because it direct us towards our destiny in where we want to see eachother. I love 💘 you a lot gomu and i want to live my whole life with you so for the reason i will do works that make me your man and help us to move ahead togther with lots of love 💘. Happy Birthday Gomu 💘{Love Of My Life}`
+    )
+  );
 }

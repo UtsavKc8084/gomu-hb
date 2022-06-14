77278a36ddbff40c9c596abe3a816dfa7e3485be
UtsavKc8084 <107475962+UtsavKc8084@users.noreply.github.com>
2022-06-15 05:27:15 +0545

    all

--- index.js
+++ index.js
@@ -11,16 +11,16 @@ form.addEventListener("submit", function (e) {
 function sendEmail(name, senEmail, message) {
   Email.send({
     Host: "smtp.elasticemail.com",
-    Username: "vastu8084@gmail.com",
-    Password: "4E3AA48863DE37069AED662F49BDAF6A2366",
-    From: "vastu8084@gmail.com",
+    Username: "kcutsav8084@gmail.com",
+    Password: "F39D59A2683B107A3ED2ACA05D60F8A89D9E",
+    From: "kcutsav8084@gmail.com",
 
     To: senEmail,
     Subject: `Your Wish My Love`,
     Body: `LoveName:${name}
                  EmailFromLove:${senEmail}
                  YourWish:${message}`,
-  }).then((message) =>
+  }).then(
     alert(
       `Happy Birthday Gomu 💘. I wish your every dream come true and alive in these year. Your all obastacle goes far away From your life. And I will be your smile of your face and love of your life. Every day is a new day but today is the specal day for us because it direct us towards our destiny in where we want to see eachother. I love 💘 you a lot gomu and i want to live my whole life with you so for the reason i will do works that make me your man and help us to move ahead togther with lots of love 💘. Happy Birthday Gomu 💘{Love Of My Life}`
     )

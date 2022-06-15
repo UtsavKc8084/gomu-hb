613afd213fd347574f91559d010724d110336def
UtsavKc8084 <107475962+UtsavKc8084@users.noreply.github.com>
2022-06-15 11:50:21 +0545

    all

--- index.js
+++ index.js
@@ -155,7 +155,7 @@ function sendEmail(name, senEmail, message) {
                 <div id="card">
                     <h1>Hey There!!!</h1>
                     <div class="image-crop">
-                        <img id="avatar" src="./gomuavatar.jpg"></img>
+                        <img id="avatar" src="./Cartoon.png"></img>
                     </div>
                     <div id="bio">
                         <p>{{CONTENT}}</p>
